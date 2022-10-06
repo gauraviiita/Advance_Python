cfb6c1edd3d6b912bdc103ab732abc8e42b32da2
Gaurav Kumar Yadav <45019238+gauraviiita@users.noreply.github.com>
2022-10-06 17:48:26 +0200

    Add files via upload

---
+++ Logger/logging_tutorial1.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%m/%d/%Y %H:%M:%S')
+import helper
---
+++ Logger/logging_tutorial6.py
@@ -0,0 +1,14 @@
+from cgitb import handler
+import logging
+import time
+from logging.handlers import TimedRotatingFileHandler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = TimedRotatingFileHandler('timed_test.log', when='s', interval=5, backupCount=5)
+logger.addHandler(handler)
+
+for _ in range(6):
+    logger.info('hello world')
+    time.sleep(5)
